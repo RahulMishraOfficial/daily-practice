f8f1381d02f96134ff7fa6c48a60d558f08c7f3b
RahulMishraOfficial <rahumishraofficial@gmail.com>
2024-06-03 12:17:35 +0530

    shallow copy and deep copy

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-const arr = ["name","name2","name3","name1","name5","name6","name18"]
-const arr1 = ["name42","name52","name5","name8"]
-const arr2 = ["name9","name11","name82","name9"]
-let at = arr.at(5)
-let concat = arr.concat(arr1,arr2)
-let copyWithin = arr.copyWithin(2,6)
-let entries = arr.entries()
-for(el of entries){
-    console.log(el)
+//SHALLOW COPY
+
+
+// let obj = {
+//     name:"karan",
+//     age: 23,
+//     city: "tarapur"
+// }
+// let obj2 = {}
+// Object.assign(obj2,obj)
+// obj.name = "ashish"
+// obj2.name = "kiran"
+// console.log(obj)
+// console.log(obj2)
+
+// let arr = ["orange","mango","papaya","cherry"]
+// let arr2 = [].concat(arr)
+// // Object.assign(arr2,arr)
+// let arr3 = arr.concat(arr2)
+// arr.push("first")
+// arr2.push("second")
+// arr3.push("third")
+// arr2.push("coconut","pear","apple")
+// console.log(arr)
+// console.log(arr2)
+// console.log(arr3)
+
+let obj = {
+    name:"karna",
+    age:23,
+    pata:{
+        city:"kanpur",
+        pinCode:625235
+    },
+    subject:["hindi","english","math","bio"]
 }
 
-// console.log(at)
-// console.log(concat)
-// console.log(copyWithin)
-// console.dir(entries)
+//SHALLOW COPY   <----- not works prefectly on nested non-premitive dataTypes 
+
+// let obj2 = {...obj}
+// Object.assign(obj2,obj)
+
+//DEEP COPY
+
+let obj2 = JSON.parse(JSON.stringify(obj));
+obj2.subject.push("computer")
+console.log(obj)
+console.log(obj2)
